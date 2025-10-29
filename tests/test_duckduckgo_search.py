3ca2ea22591c7e74c622c8f92ecde406a473b960
EjBir <ejbiriukov@gmail.com>
2025-10-29 12:36:16 -0400

    added headless mode

--- tests/test_duckduckgo_search.py
+++ tests/test_duckduckgo_search.py
@@ -17,7 +17,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 @pytest.fixture
 def driver():
     options = Options()
-    #options.add_argument("--headless")
+    options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     service = Service(ChromeDriverManager().install())
